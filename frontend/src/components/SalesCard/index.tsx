d8663baa8891750ef5367d650aba69cd3efc96b6
Alvaro Gabriel <agsa0108@gmail.com>
2022-07-13 11:31:34 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,3 +1,5 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 
@@ -7,10 +9,20 @@ function SalesCard() {
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
               <div className="dsmeta-form-control-container">
-                <input className="dsmeta-form-control" type="text"/>
+                <DatePicker
+                    selected={new Date()}
+                    onChange={(date: Date) => {}}
+                    className="dsmeta-form-control"
+                    dateFormat="dd/MM/yyyy"
+                 />
               </div>
               <div className="dsmeta-form-control-container">
-                <input className="dsmeta-form-control" type="text"/>
+                <DatePicker
+                    selected={new Date()}
+                    onChange={(date: Date) => {}}
+                    className="dsmeta-form-control"
+                    dateFormat="dd/MM/yyyy"
+                />
               </div>
             </div>
 
@@ -43,7 +55,7 @@ function SalesCard() {
                   </tr>
                   <tr>
                     <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
+                    <td className="show576">09/07/2022</td>
                     <td>Anakin</td>
                     <td className="show992">15</td>
                     <td className="show992">11</td>
@@ -56,7 +68,7 @@ function SalesCard() {
                   </tr>
                   <tr>
                     <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
+                    <td className="show576">10/07/2022</td>
                     <td>Anakin</td>
                     <td className="show992">15</td>
                     <td className="show992">11</td>

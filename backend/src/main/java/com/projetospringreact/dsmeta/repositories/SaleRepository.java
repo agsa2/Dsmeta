86820a38152b30ed8ec1568c28ed7499269d93e1
Alvaro Gabriel <agsa0108@gmail.com>
2022-07-14 12:21:47 -0300

    API test

---
+++ backend/src/main/java/com/projetospringreact/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.projetospringreact.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetospringreact.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
